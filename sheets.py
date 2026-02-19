b3184807d72941565cdb5e9ca4dbce87db26e8be
Ishiqa Patel <ishiqa785@Ishiqas-MacBook-Air.local>
2026-02-19 16:05:53 +0530

    Final fix sheets.py using Streamlit secrets

--- sheets.py
+++ sheets.py
@@ -1,44 +1,29 @@
-import json
 import streamlit as st
+import json
 import gspread
-import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
-
+from google.oauth2.service_account import Credentials
 
 # Define scope
 scope = [
-    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-
 
 # Load credentials from Streamlit secrets
 creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
 
-import streamlit as st
-from google.oauth2.service_account import Credentials
-import gspread
-
-def get_client():
-    creds = Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
-    )
-    return gspread.authorize(creds)
-
+creds = Credentials.from_service_account_info(
+    creds_dict,
+    scopes=scope
 )
 
-
-# Authorize client
 client = gspread.authorize(creds)
-
 
 # Spreadsheet name
 SPREADSHEET_NAME = "DroneOps"
-
 
 # Function to read sheet
 def read_sheet(sheet_name):
     sheet = client.open(SPREADSHEET_NAME).worksheet(sheet_name)
     data = sheet.get_all_records()
-    return pd.DataFrame(data)
+    return data
